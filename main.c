55c639b48681060029cb3526d05d4b502ebe2ecb
christoph-karpowicz <christoph.karpowicz@gmail.com>
2022-07-28 22:41:56 +0200

    Add button press handling.

--- main.c
+++ main.c
@@ -1,19 +1,23 @@
 #ifndef F_CPU
-#define F_CPU 1000000UL // or whatever may be your frequency
+#define F_CPU 1000000UL
 #endif
 
 #include <avr/io.h>
 #include <util/delay.h>
-#define MY_PORT PORTD
-#define MY_DDR DDRD
+
+#define KEY_PRESSED !(PINC & (1 << PC0))
 
 int main(void) {
-    MY_DDR |= 0xFF;
+    DDRD |= 0xFF;
+    // DDRC |= (1 << PC1);
     while (1) {
-        MY_PORT = 0x00;
-        _delay_ms(500);
-        MY_PORT = 0xFF;
-        _delay_ms(500);
+        if (KEY_PRESSED) {
+            PORTD = 0xFF;
+            // PORTC |= (1 << PC1);
+        } else {
+            PORTD = 0x00;
+            // PORTC &= !(1 << PC1);
+        }
     }
     return 0;
 }
