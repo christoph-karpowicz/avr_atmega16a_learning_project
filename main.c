9a2cfccac64808fe89a9465cea7fba2d05c3d8c5
christoph-karpowicz <christoph.karpowicz@gmail.com>
2022-08-02 22:04:46 +0200

    Create Makefile, blink LED every ~4 sec using a timer interrupt.

--- main.c
+++ main.c
@@ -6,16 +6,39 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+volatile uint8_t timer;
+
 ISR(INT0_vect) {
     PORTC |= (1 << PC0);
 }
 
+ISR(TIMER0_COMP_vect) {
+    timer++;
+}
+
 int main(void) {
     DDRC |= 0xFF;
+
+    // CTC mode for Timer0
+    TCCR0 |= (1 << WGM01);
+    // 64 timer divider
+    TCCR0 |= (1 << CS00) | (1 << CS01);
+    // Timer's top value 
+    OCR0 = 255;
+    // Turn on the Timer/Counter0 Compare Match interrupt
+    TIMSK |= (1 << OCIE0);
+
+    // Turn on interrupts on pin INT0
     GICR |= (1 << INT0);
+
     sei();
     while (1) {
-        PORTC &= !(1 << PC0);
+        if (timer == 0) {
+            PORTC |= (1 << PC0);
+            _delay_ms(100);
+        } else {
+            PORTC &= !(1 << PC0);
+        }
     }
     return 0;
 }
