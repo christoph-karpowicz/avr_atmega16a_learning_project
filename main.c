396266f3e2278fa90d7abd1b5c0281ee561623ce
christoph-karpowicz <christoph.karpowicz@gmail.com>
2022-09-14 19:35:05 +0200

    Detect motion with a PIR HC-SR501 sensor.

--- main.c
+++ main.c
@@ -6,11 +6,34 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+#define MOTION_DETECTED (PINA & (1 << PA0))
+
 volatile uint8_t timer;
 
-ISR(INT0_vect) {
+void yellow_led_on() {
     PORTC |= (1 << PC0);
 }
+
+void yellow_led_off() {
+    PORTC &= !(1 << PC0);
+}
+
+void green_led_on() {
+    PORTC |= (1 << PC1);
+}
+
+void green_led_off() {
+    PORTC &= !(1 << PC1);
+}
+
+ISR(INT0_vect) {
+    yellow_led_on();
+}
+
+// ISR(INT1_vect) {
+//     led_on();
+//     _delay_ms(1000);
+// }
 
 ISR(TIMER0_COMP_vect) {
     timer++;
@@ -18,6 +41,7 @@ ISR(TIMER0_COMP_vect) {
 
 int main(void) {
     DDRC |= 0xFF;
+    DDRA |= 0;
 
     // CTC mode for Timer0
     TCCR0 |= (1 << WGM01);
@@ -30,15 +54,27 @@ int main(void) {
 
     // Turn on interrupts on pin INT0
     GICR |= (1 << INT0);
+    // Turn on interrupts on pin INT1
+    // GICR |= (1 << INT1);
+
+    // Generate INT1 interrupt on rising egde
+    // MCUCR |= (1 << ISC11) | (1 << ISC10);
 
     sei();
+    _delay_ms(2000);
     while (1) {
         if (timer == 0) {
-            PORTC |= (1 << PC0);
+            yellow_led_on();
             _delay_ms(100);
         } else {
-            PORTC &= !(1 << PC0);
+            yellow_led_off();
         }
+        if (MOTION_DETECTED) {
+            green_led_on();
+        } else {
+            green_led_off();
+        }
+        yellow_led_off();
     }
     return 0;
 }
